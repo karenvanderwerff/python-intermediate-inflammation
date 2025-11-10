b5351cfe105089110abffce8dc1265116adc5d98
karen <k.vanderwerff@erasmusmc.nl>
2025-11-10 12:17:17 +0100

    Reformatting

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,38 +6,45 @@ import pytest
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
-    ])
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ],
+)
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-@pytest.mark.parametrize(
-    "test, expected",
-    [
-        ([ [0, -5, 2], [0, 3, -4], [0, -5, 6] ], [0, 3, 6]),
-        ([ [0, -5, 2], [0, 3, -4], [0, -5, 10.5] ], [0, 3, 10.5]),
-    ])
-def test_daily_max(test, expected):
-    """Test max function works for array of zeroes and positive integers."""
-    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, -5, 2], [0, 3, 4], [0, -5, 6]] , [0, -5, 2]),
-        ([ [0, -5, 2], [0, 3, 4], [0, -5.5, 6.5] ], [0, -5.5, 2]),
-    ])
+        ([[0, -5, 2], [0, 3, -4], [0, -5, 6]], [0, 3, 6]),
+        ([[0, -5, 2], [0, 3, -4], [0, -5, 10.5]], [0, 3, 10.5]),
+    ],
+)
+def test_daily_max(test, expected):
+    """Test max function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, -5, 2], [0, 3, 4], [0, -5, 6]], [0, -5, 2]),
+        ([[0, -5, 2], [0, 3, 4], [0, -5.5, 6.5]], [0, -5.5, 2]),
+    ],
+)
 def test_daily_min(test, expected):
     """Test min function works for array of zeroes and positive integers."""
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
 
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
 
     with pytest.raises(TypeError):
-        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+        error_expected = daily_min([["Hello", "there"], ["General", "Kenobi"]])
