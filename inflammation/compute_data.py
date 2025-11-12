836ffdc733b768b71a9ae15c0f0ad34b5410c3bb
karen <k.vanderwerff@erasmusmc.nl>
2025-11-12 14:51:54 +0100

    Remove visualization and test output

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -32,19 +32,6 @@ class JSONDataSource:
         return list(data)
 
 
-def load_inflammation_data(data_dir):
-    """Loads inflammation data from CSV files within the given directory.
-
-    :param data_dir: Directory to be searched for inflammation CSV files.
-    :returns: List of 2D NumPy arrays with inflammation data from all CSV files.
-    """
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-    return list(data)
-
-
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
@@ -60,7 +47,8 @@ def analyse_data(data_source):
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
 
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+    # graph_data = {
+    #     'standard deviation by day': daily_standard_deviation,
+    # }
+    # views.visualize(graph_data)
+    return daily_standard_deviation
--- tests/test_compute_data.py
+++ tests/test_compute_data.py
@@ -1,5 +1,8 @@
 from unittest.mock import Mock, patch
+import numpy.testing as npt
+import os
 
+from inflammation.compute_data import analyse_data, CSVDataSource
 
 def test_analyse_data_mock_source():
     from inflammation.compute_data import analyse_data
@@ -7,6 +10,23 @@ def test_analyse_data_mock_source():
     data_source = Mock()
     data_source.load_inflammation_data.return_value = [[[1, 2, 3]], [[4, 5, 6]]]
 
-    # Patch the visualization so it doesn't block the test
-    with patch("inflammation.compute_data.views.visualize"):
-        analyse_data(data_source)
+    analyse_data(data_source)
+
+    # # Patch the visualization so it doesn't block the test
+    # with patch("inflammation.compute_data.views.visualize"):
+        # analyse_data(data_source)
+
+
+def test_analyse_data():
+    path = os.path.dirname('data/')
+    data_source = CSVDataSource(path)
+    result = analyse_data(data_source)
+    expected = [0.,0.22510286,0.18157299,0.1264423,0.9495481,0.27118211,
+                       0.25104719,0.22330897,0.89680503,0.21573875,1.24235548,0.63042094,
+                       1.57511696,2.18850242,0.3729574,0.69395538,2.52365162,0.3179312,
+                       1.22850657,1.63149639,2.45861227,1.55556052,2.8214853,0.92117578,
+                       0.76176979,2.18346188,0.55368435,1.78441632,0.26549221,1.43938417,
+                       0.78959769,0.64913879,1.16078544,0.42417995,0.36019114,0.80801707,
+                       0.50323031,0.47574665,0.45197398,0.22070227]
+    npt.assert_array_almost_equal (result, expected)
+
