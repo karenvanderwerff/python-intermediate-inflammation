b537bb914c2c8f3f7c97cdfaefc4935a56a3ae9a
karen <k.vanderwerff@erasmusmc.nl>
2025-11-12 14:57:34 +0100

    Refactoring using a pure function

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -41,14 +41,18 @@ def analyse_data(data_source):
     :param data_dir: Directory that contains the inflammation CSV files.
     """
     data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
 
+    graph_data = {
+        'standard deviation by day': daily_standard_deviation,
+    }
+    # views.visualize(graph_data)
+    return daily_standard_deviation
+
+
+def compute_standard_deviation_by_day(data):
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
-    # graph_data = {
-    #     'standard deviation by day': daily_standard_deviation,
-    # }
-    # views.visualize(graph_data)
     return daily_standard_deviation
